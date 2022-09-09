80e9731669915e7ac7c56ebfd42d01621994028b
Adetoroye Oluleke Moses <adetleke@gmail.com>
2022-09-09 12:23:42 +0000

    ...

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdi0.h>
+/**
+ *main - entry point
+ *
+ * Return: 0 after printing the function
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
