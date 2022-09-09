57eeb791ab599f07977a1fd751a61c01d180f013
Adetoroye Oluleke Moses <adetleke@gmail.com>
2022-09-09 20:23:56 +0000

    ...

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Zero 0 to show success
+ */
+
+int main(void)
+{
+	int a;
+	long int b;
+	long long int c;
+	char d;
+	float f;
+
+	printf("size of char: %lu byte(s)\n", (unsigned long)sizeof(d));
+	printf("size of n int: %lu byte(s)\n", (unsigned long)sizeof(a));
+	printf("size of a long int: %lu byte(s)\n", (unsigned long )sizeof(b));
+	printf("size of long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("size of float: %lu byte(s)\n", (unsigned long)sizeof(f));
+	return: (0);
+}
