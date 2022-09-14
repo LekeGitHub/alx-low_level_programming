37a776e0bda852df3e851abcac8eb846bd019ebc
Adetoroye Oluleke Moses <adetleke@gmail.com>
2022-09-14 23:02:14 +0000

    ..

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - fnction
+ * print_alphabet_x10 - function that will print the alphabet
  *
  */
 void print_alphabet_x10(void)
@@ -10,8 +10,7 @@ void print_alphabet_x10(void)
 
 	for (line = 0; line <= 9; ++line)
 	{
-
-		for (ch = 'a'; line <= 'z'; ++ch)
+		for (ch = 'a'; ch <= 'z'; ++ch)
 			_putchar(ch)
 		_putchar('\n');
 	}
