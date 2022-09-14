91871abbac56223e5d0942bced1c378c779fc010
Adetoroye Oluleke Moses <adetleke@gmail.com>
2022-09-14 22:23:30 +0000

    ...

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,8 @@
-#include "main.h"
+#include"main.h"
+
 /**
- * Main - Entry Point
- *
- * print_alphabet - utilizes on the _putchar function to print the alphabet a - z
- *
+ * print_alphabet - utilizes on the _putchar function to print
+ *                 the alphabet a - z
  */
 
 void print_alphabet(void)
