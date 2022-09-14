d8a82563693b4101cc3a9dd3844adc1a55689fae
Adetoroye Oluleke Moses <adetleke@gmail.com>
2022-09-14 21:45:41 +0000

    ...

---
+++ 0x02-functions_nested_loops/1_alphabet.c
@@ -0,0 +1,19 @@
+#include <main.h>
+/**
+ * Main - Entry Point
+ *
+ * Description: A code to print alphabet in lowercase
+ *
+ * Author: Adetoroye Oluleke Moses
+ *
+ * Return: 0
+ */
+
+void print_alphabet(void);
+{
+	int ch;
+
+	for(ch = 'a'; ch <= 'z'; ++ch)
+		_putchar(ch);
+	_putchar('\n');
+}
