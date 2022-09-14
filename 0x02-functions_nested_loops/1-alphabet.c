b89545b9e19db1462cf6d2f275380730057939ed
Adetoroye Oluleke Moses <adetleke@gmail.com>
2022-09-14 22:01:57 +0000

    ...

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -9,11 +9,11 @@
  * Return: 0
  */
 
-void print_alphabet(void);
+void print_alphabet(void)
 {
 	int ch;
 
-	for(ch = 'a'; ch <= 'z'; ++ch)
+	for (ch = 'a'; ch <= 'z'; ++ch)
 		_putchar(ch);
 	_putchar('\n');
 }
