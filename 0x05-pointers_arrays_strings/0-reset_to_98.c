423590e587d29579c6b9797fef61a48e10f65e20
Adetoroye Oluleke Moses <adetleke@gmail.com>
2022-09-20 04:11:10 +0000

    swap two int

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - function that takes a pointer to an int as parameter and updates the value it points to
+ *
+ * Description: Pointer Function
+ *
+ * Return: 0
+ */
+
+void reset_to_98(int *n);
+{
+	*n = 98;	
+}
