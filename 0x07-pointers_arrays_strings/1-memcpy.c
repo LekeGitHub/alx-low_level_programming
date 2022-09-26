24d8901e7709827c42f50953f82d99f317b11739
Adetoroye Oluleke Moses <adetleke@gmail.com>
2022-09-26 19:50:48 +0000

    ...

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * char*_memcpy - copies n bytes from memory area scr to memory area dest
+ *
+ * @dest: destination
+ * @src: source
+ * @n: number oof bytes
+ *
+ * Return: pointer to dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n);
+{
+	int size = n;
+
+	if (size > 0)
+	{
+		int i;
+
+		for (i = 0; i < size; i++)
+			dest[i] = src[i];
+	}
+	return (dest);
+}
